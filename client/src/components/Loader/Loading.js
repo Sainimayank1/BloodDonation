57e8bf423b4a46ef35e9f4b912c4decf4bbf10b6
Mayank <mayanksaini4455@gmail.com>
2023-03-24 20:03:35 +0530

    Final

---
+++ client/src/components/Loader/Loading.js
@@ -0,0 +1,11 @@
+import "./loading.css"
+
+function Loading() {
+  return (
+    <div className="loader_container">
+      <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default Loading
